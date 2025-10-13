2eae7b07cbb233c6dae5a73eff4f6103ebf687b5
thauanb-dev <thauanb@gmail.com>
2025-10-13 20:54:40 -0300

    feat: Esqueleto do Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+function Navbar(){
+    return(
+    <div className='font-sans w-full h-12 flex justify-between items-center gap-3 border-solid border-b-[0.1px] border-stone-700 '>
+        <p>a</p>
+        <p>b</p>
+        <p>tema</p>
+    </div>
+    )
+}
+
+export default Navbar
