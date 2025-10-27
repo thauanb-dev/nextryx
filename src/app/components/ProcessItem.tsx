f2faac45b36b168692ed9e9a80c963e6b0f95d17
thauanb-dev <thauanb@gmail.com>
2025-10-26 22:35:59 -0300

    feat: `Copy` para processos

--- src/app/components/ProcessItem.tsx
+++ src/app/components/ProcessItem.tsx
@@ -1,9 +1,11 @@
 "use client"
 import '../layout.css'
-import {Trash , Plus } from 'lucide-react'
-interface ProcessItemProps{
+import {Trash , Copy } from 'lucide-react'
+
+export interface  ProcessItemProps{
     name : string
-    key: string
+    onDelete: () => void //evento personalizado para deletar
+    onCopy: () => void //evento personalizado para copiar  
 }
 
 //TODO - Criar uma classe?/atalho no tailwindcss para simplificar as classes repetidas
@@ -14,16 +16,35 @@ interface ProcessItemProps{
 //TODO - Tornar os processo arrastaveis
 //TODO - Melhorar a experiencia UI
 
-function ProcessItem({name,key}:ProcessItemProps){
-    return(
-            <div className="flex h-[50px] w-[250px] border-l-8 border-green-500 bg-emerald-50 text-stone-900 rounded-xl hover:shadow-neutral-300 hover:shadow-md  transition-all duration-300 cursor-pointer items-center p-1 gap-2 ">
-                <div className="flex border-slate-300 border-r-2 p-1">
-                    <Trash  onClick={(e)=>{console.log(e)}}/>
-                </div>
-                <p>{name}</p>
-            </div>
-
-    )
+function ProcessItem({ name, onDelete, onCopy }: ProcessItemProps) {
+  return (
+    <div
+      className="flex h-[50px] w-[250px] border-l-8 border-green-500 bg-emerald-50 text-stone-900 rounded-xl hover:shadow-neutral-300 hover:shadow-md  transition-all duration-300 cursor-pointer items-center p-1 gap-2 "
+      onClick={(e) => {
+        console.log(e);
+      }}
+    >
+      <div className="flex border-slate-300 border-r-2 p-1">
+        <Trash
+          size={16}
+          height={16}
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        />
+        <Copy
+          size={16}
+          height={16}
+          onClick={(e) => {
+            e.stopPropagation();
+            onCopy();
+          }}
+        />
+      </div>
+      <p>{name}</p>
+    </div>
+  );
 }
 
 export default ProcessItem
--- src/app/components/ProcessList.tsx
+++ src/app/components/ProcessList.tsx
@@ -1,38 +1,55 @@
 "use client"
 import ProcessItem from "./ProcessItem"
+import {ProcessItemProps} from "./ProcessItem"
 import { useProcess } from "./context/ProcessContext"
-import { useEffect } from "react"
+import { useEffect , useState } from "react"
 //tipar processList
+export type Process = {
+  name: string;
+  key: string;
+};
+
 function ProcessList(){
-    const processos = [
-  { "name": "001.2025.123.556-78", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-79", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-80", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-81", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-82", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-83", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-84", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-85", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-86", "key": crypto.randomUUID() },
-  { "name": "001.2025.123.556-87", "key": crypto.randomUUID() }
-]
+const [process, setProcess] = useState<Process[]>([
+  { name: "001.2025.123.556-78", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-79", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-80", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-81", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-82", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-83", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-84", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-85", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-86", key: crypto.randomUUID() },
+  { name: "001.2025.123.556-87", key: crypto.randomUUID() },
+]);
   //utilizando o hook personalizado do ProcessContext
   const {setTotal} = useProcess() 
 
-  // Atualiza o valor global sempre que a lista mudar
-    useEffect(()=>{
-        setTotal(processos.length)
-    },[processos,setTotal])
+const handleDelete = (key:string)=>{
+    setProcess((prev) => prev.filter((p) => p.key !== key));
+}
+const handleCopy = (name:string)=>{
+   navigator.clipboard.writeText(name)
+   console.log("Copiado:", name)
+}
 
-    return(
-        <div className="m-3 flex flex-wrap  gap-4 min-w-[80vw] min-h-[20vh] p-2 ">
-        {
-            processos.map((p)=>(
-                <ProcessItem key={p.key.toString()} name={p.name}/>
-            ))
-        }
-        </div>
-    )
+  // Atualiza o valor global sempre que a lista mudar
+    useEffect(() => {
+      setTotal(process.length);
+    }, [process, setTotal]);
+
+    return (
+      <div className="m-3 flex flex-wrap  gap-4 min-w-[80vw] min-h-[20vh] p-2 ">
+        {process.map((p) => (
+          <ProcessItem 
+            key={p.key.toString()} 
+            name={p.name} 
+            onDelete={() => handleDelete(p.key)}
+            onCopy={() => handleCopy(p.name)}
+            />
+        ))}
+      </div>
+    );
 }
 
 export default ProcessList
